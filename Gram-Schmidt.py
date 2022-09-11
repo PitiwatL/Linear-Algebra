0e35b1face47fab08efac30203e2c2f1c85305bc
Pitiwat Lueangwitchajaroen <81637352+PitiwatL@users.noreply.github.com>
2022-09-12 03:16:49 +0700

    Update Gram-Schmidt.py

--- Gram-Schmidt.py
+++ Gram-Schmidt.py
@@ -8,17 +8,15 @@ verySmallNumber = 1e-14
 
 def gsBasis4(A) :
     B = np.array(A, dtype=np.float_) 
-    B[:, 0] = B[:, 0] / la.norm(B[:, 0]) #Zero column
+    B[:, 0] = B[:, 0] / la.norm(B[:, 0]) # Zero column
     
-    B[:, 1] = B[:, 1] - B[:, 1] @ B[:, 0] * B[:, 0]
+    B[:, 1] = B[:, 1] - B[:, 1] @ B[:, 0] * B[:, 0] # First column
     
     if la.norm(B[:, 1]) > verySmallNumber :
         B[:, 1] = B[:, 1] / la.norm(B[:, 1])
-        
     else :
         B[:, 1] = np.zeros_like(B[:, 1]) 
     
-
     B[:, 2] = B[:, 2] - B[:, 2] @ B[:, 0] * B[:, 0] - B[:, 2] @ B[:, 1] * B[:, 1]
     
     if la.norm(B[:, 2]) > verySmallNumber :
@@ -42,7 +40,7 @@ def gsBasis(A) :
             B[:, 0] = B[:, 0] / la.norm(B[:, 0])
         if i > 0 :
             for j in range(i) :
-                B[:, i] -= B[:, i]@B[:, i-1] * B[:, i-1]     
+                B[:, i] -= B[:, i]@B[:, j] * B[:, j]     
             if la.norm(B[:, i]) > verySmallNumber :
                 B[:, i] = B[:, i] / la.norm(B[:, i])
             else :
